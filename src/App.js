143cb27ed03150028c0f62b8b814d2aecd5f5722
Jonathan Freihofer <patientfrei@gmail.com>
2021-08-29 19:23:16 -0400

    converted home page component

--- src/App.js
+++ src/App.js
@@ -1,52 +1,14 @@
 /* eslint-disable no-restricted-globals */
 import "./App.css";
-import { React, useState } from "react";
-import LinkText from "../src/components/LinkText";
-import { FaEye, FaCode, FaClipboard } from "react-icons/fa";
+import React from "react";
+import Home from "../src/pages/Home";
+
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
-function App({ position }) {
-  const [hovered, setHovered] = useState(false);
-  const [title, setTitle] = useState("");
-
+function App() {
   return (
     <>
-      <div className="landing__page">
-        <header className={"name"}>Jonathan Freihofer</header>
-        <br />
-        <p className="subtitle">Front End Engineer</p>
-        <main className="icon__container">
-          <FaEye
-            className="icon about"
-            onMouseEnter={() => {
-              setHovered(true);
-              setTitle("About");
-            }}
-            onMouseLeave={() => setHovered(false)}
-          />
-          <FaCode
-            className="icon project"
-            onMouseEnter={() => {
-              setHovered(true);
-              setTitle("Recent Projects");
-            }}
-            onMouseLeave={() => setHovered(false)}
-          />
-          <FaClipboard
-            className="icon resume"
-            onMouseEnter={() => {
-              setHovered(true);
-              setTitle("Resume");
-            }}
-            onMouseLeave={() => setHovered(false)}
-          />
-        </main>
-        <LinkText hovered={hovered}>
-          <div className="titles">
-            <p className="icon-title">{hovered && title}</p>
-          </div>
-        </LinkText>
-      </div>
+      <Home />
     </>
   );
 }
---
+++ src/pages/Home/index.js
@@ -0,0 +1,50 @@
+import { React, useState } from "react";
+import LinkText from "../../components/LinkText";
+import { FaEye, FaCode, FaClipboard } from "react-icons/fa";
+import { BrowserRouter as Router, Link, Switch, Route } from "react-router-dom";
+
+export default function Home({ children }) {
+  const [hovered, setHovered] = useState(false);
+  const [title, setTitle] = useState("");
+
+  return (
+    <>
+      <div className="landing__page">
+        <header className={"name"}>Jonathan Freihofer</header>
+        <br />
+        <p className="subtitle">Front End Engineer</p>
+        <main className="icon__container">
+          <FaEye
+            className="icon about"
+            onMouseEnter={() => {
+              setHovered(true);
+              setTitle("About");
+            }}
+            onMouseLeave={() => setHovered(false)}
+          />
+          <FaCode
+            className="icon project"
+            onMouseEnter={() => {
+              setHovered(true);
+              setTitle("Recent Projects");
+            }}
+            onMouseLeave={() => setHovered(false)}
+          />
+          <FaClipboard
+            className="icon resume"
+            onMouseEnter={() => {
+              setHovered(true);
+              setTitle("Resume");
+            }}
+            onMouseLeave={() => setHovered(false)}
+          />
+        </main>
+        <LinkText hovered={hovered}>
+          <div className="titles">
+            <p className="icon-title">{hovered && title}</p>
+          </div>
+        </LinkText>
+      </div>
+    </>
+  );
+}
