4d706216e69613daa0a618d8f070d0ee018a580f
Jonathan Freihofer <patientfrei@gmail.com>
2021-09-11 21:38:42 -0400

    yarn

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 /* eslint-disable no-restricted-globals */
 import "./App.css";
 import React from "react";
+import About from "../src/pages/About";
 import Home from "../src/pages/Home";
 
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
@@ -10,10 +11,12 @@ function App() {
     <>
       <Router>
         <Switch>
-          <Route exact patch="/">
+          <Route exact path="/">
             <Home />
           </Route>
-          <Route path="/about"></Route>
+          <Route path="/about">
+            <About />
+          </Route>
           <Route path="/projects"></Route>
           <Route path="/resume"></Route>
         </Switch>
