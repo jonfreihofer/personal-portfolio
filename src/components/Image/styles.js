17b151956488141b72b5a8b211cff8ef75825429
Jonathan Freihofer <patientfrei@gmail.com>
2021-10-02 12:45:59 -0400

    projects page build out

--- src/components/Image/styles.js
+++ src/components/Image/styles.js
@@ -1,13 +1,22 @@
 import styled from "styled-components";
+import "../../App.css";
+import "../../index.css";
 
 export const StyledImage = styled.img`
   width: 300px;
   height: 300px;
+  margin: 12px;
   border-radius: 15px;
 `;
 
 export const ImageWrapper = styled.div`
-  border: solid 3px pink;
+  margin: 0 auto;
+  background: var(--color-main-blue);
+  box-shadow: var(--global-box-shadow);
+  display: flex;
+  text-align: center;
+  justify-content: space-between;
   border-radius: 15px;
-  width: 300px;
+  height: 100vh;
+  width: auto;
 `;
--- src/pages/Projects/index.js
+++ src/pages/Projects/index.js
@@ -4,11 +4,23 @@ import { StyledImage, ImageWrapper } from "../../components/Image/styles.js";
 export default function Projects() {
   return (
     <>
-      <ImageWrapper>
-        <StyledImage src="./screenshot-of-contacts.png" alt="boh" />
-        <StyledImage src="./screenshot-of-synth.png" alt="boh" />
-        <StyledImage src="./synthpic.png" alt="boh" />
-      </ImageWrapper>
+      <div className="projects-container">
+        <h1>Recent Projects I've Worked On</h1>
+        <ImageWrapper>
+          <div className="project">
+            <h3>Contacts Manager</h3>
+            <StyledImage src="./screenshot-of-contacts.png" alt="boh" />
+          </div>
+          <div className="project">
+            <h3>Snthrgy Browser Synth</h3>
+            <StyledImage src="./screenshot-of-synth.png" alt="boh" />
+          </div>
+          <div className="project">
+            <h3>Hyper Todo</h3>
+            <StyledImage src="./screenshot-of-todo.png" alt="boh" />
+          </div>
+        </ImageWrapper>
+      </div>
     </>
   );
 }
